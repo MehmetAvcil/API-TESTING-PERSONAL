da9030d2516f822bee0de66b1938d52d4e65bea9
Mehmet Avcil <mehmetbaranavcil@gmail.com>
2026-06-09 12:54:21 +0100

    Updated config files and base specs for Rest.

--- src/test/java/utilities/ApiUtils.java
+++ src/test/java/utilities/ApiUtils.java
@@ -1,20 +1,35 @@
 package utilities;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 public class ApiUtils {
 
+    public static final String BASE_URI = "http://localhost:8080/";
+    public static final String USERNAME = "sparta";
+    public static final String PASSWORD = "global";
+
     public static final String LOGIN_PATH = "/Auth/login";
-    public static final String SPARTANS_PATH = "/api/spartans";
-    public static final String COURSES_PATH = "/api/Courses";
+    public static final String SPARTANS_PATH = "/Spartans";
+    public static final String COURSES_PATH = "/Courses";
 
     public static RequestSpecification getBaseRequestSpec() {
-        return RestAssured.given()
-                .baseUri(ConfigurationReader.getProperty("base_url"))
-                .contentType(ContentType.JSON)
-                .log().all();
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(LOGIN_PATH)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static RequestSpecification getBearerRequestSpec(String token) {
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath("/api")
+                .setContentType(ContentType.JSON)
+                .addHeader("Authorization", "Bearer " + token)
+                .build();
     }
 
 }
