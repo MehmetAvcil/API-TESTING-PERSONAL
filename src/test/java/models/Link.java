8328200cbec59ac5b049931db5765b52a77b809a
Mehmet Avcil <mehmetbaranavcil@gmail.com>
2026-06-09 14:17:34 +0100

    Added POJOs for Course and Spartan responses.

---
+++ src/test/java/models/Link.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Link {
+    private String href;
+    private String rel;
+    private String method;
+
+    public Link() {}
+
+    // getters
+    public String getHref() { return href; }
+    public String getRel() { return rel; }
+    public String getMethod() { return method; }
+
+    // setters
+    public void setHref(String href) { this.href = href; }
+    public void setRel(String rel) { this.rel = rel; }
+    public void setMethod(String method) { this.method = method; }
+}
